2153fe4732436ac095cfa3dd430d376b38143f9a
Taewan-P <swimingtw@gmail.com>
2020-04-11 22:51:35 +0900

    Add diameter of binary tree solution in Java
    https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/529/week-2/3293/

---
+++ 30_day_leetcoding_challenge/week_2/diameter_of_binary_tree.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int diameterOfBinaryTree(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int lh = height(root.left);
+        int rh = height(root.right);
+
+        int ld = diameterOfBinaryTree(root.left);
+        int rd = diameterOfBinaryTree(root.right);
+
+        return Math.max(lh + rh, Math.max(ld,rd));
+    }
+
+    static int height(TreeNode node) {
+        
+        if (node == null) {
+            return 0;
+        }
+
+        return (1 + Math.max(height(node.left), height(node.right)));
+    }
+}
