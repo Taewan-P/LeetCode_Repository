6d1c293a83339b2b44647ac707b2105530adb6de
Taewan Park <swimingtw@gmail.com>
2020-07-08 18:27:31 +0900

    Edit solution to be more simple

--- summer_coding/58_length_of_last_word.py
+++ summer_coding/58_length_of_last_word.py
@@ -1,3 +1,3 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        return len(' '.join(s.split(" ")).split()[-1]) if ' '.join(s.split(" ")).split() != [] else 0
+        return len(s.split()[-1]) if s.split() != [] else 0
