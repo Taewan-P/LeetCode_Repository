276387c314d397e266cea5ec9ac95ef3f52c494f
Taewan Park <swimingtw@gmail.com>
2020-07-13 20:33:00 +0900

    Add reverse linked list solution in Python

---
+++ summer_coding/206_reverse_linked_list.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        cur = head
+        tmp = []
+        while cur != None:
+            tmp.append(cur.val)
+            cur = cur.next
+            
+        tmp.reverse()
+
+        for i in range(len(tmp)):
+            if i == 0:
+                ans = ListNode(tmp[i])
+                cur = ans
+            else:
+                cur.next = ListNode(tmp[i])
+                cur = cur.next
+
+        if tmp:
+            return ans
