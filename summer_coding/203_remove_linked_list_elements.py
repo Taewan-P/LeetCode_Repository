fdebc8f41e5e73ee1154b51edb0b34f66cac88c8
Taewan Park <swimingtw@gmail.com>
2020-07-14 22:22:51 +0900

    Add remove linked list elements solution in Python

---
+++ summer_coding/203_remove_linked_list_elements.py
@@ -0,0 +1,37 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        if head == None:
+            return None
+        
+        cur = head
+        while cur:
+            if cur.val == val:
+                # When the first value of ListNode is equal to the element that we're trying to remove
+                head = head.next
+            else:
+                if cur.next:
+                    # If next node is available
+                    if cur.next.val == val:
+                        # If next value if the element
+                        if cur.next.next:
+                            # If next next value is available, link current node with next next node.
+                            cur.next = cur.next.next
+                            continue
+                        else:
+                            # There is no next value, so cut linked list.
+                            cur.next = None
+                else:
+                    # The only and last element in the linked list
+                    if cur.val == val:
+                        return None
+            
+            cur = cur.next
+            
+        
+                
+        return head
