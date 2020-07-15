a1ca5ae0b1905571612e8237422081d8a5415d3e
Taewan Park <swimingtw@gmail.com>
2020-07-15 22:14:40 +0900

    Add palindrome linked list solution in Python

---
+++ summer_coding/234_palindrome_linked_list.py
@@ -0,0 +1,32 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        node = []
+        while head:
+            node.append(head.val)
+            head = head.next
+            
+        l = len(node)
+        if l%2 == 0:
+            # Even elements
+            if not node:
+                return True
+            
+            a = node[:int(l/2)]
+            b = node[int(l/2):]
+            b.reverse()
+            
+            return a == b
+            
+        else:
+            # Odd elements
+            
+            a = node[:int(l//2)]
+            b = node[int((l//2)+1):]
+            b.reverse()
+            
+            return a == b
