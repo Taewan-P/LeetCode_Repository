fe34f3463f011eaf7e8de1ea3849a186c57e9f83
Taewan Park <swimingtw@gmail.com>
2020-07-16 21:39:15 +0900

    Add same tree solution in Python

---
+++ summer_coding/100_same_tree.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        return self.treeToList(p) == self.treeToList(q)
+        
+        
+    def treeToList(self, tree: TreeNode) -> List[int]:
+        node = []
+        if not tree:
+            return []
+        node.append(tree.val)
+        
+        if tree.left:
+            node.append(self.treeToList(tree.left))
+        else:
+            node.append(None)
+
+        if tree.right:
+            node.append(self.treeToList(tree.right))
+        else:
+            node.append(None)
+                
+        return node
