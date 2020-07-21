80d37677efd7f31e19d411f7b723b6334be65208
Taewan Park <swimingtw@gmail.com>
2020-07-21 22:43:34 +0900

    Add top k frequent elements solution in Python

---
+++ summer_coding/347_top_k_frequent_elements.py
@@ -0,0 +1,13 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        occ = dict()
+        for n in nums:
+            if n in occ:
+                occ[n] += 1
+            else:
+                occ[n] = 1
+                
+        items = list(occ.items())  
+        top_items = sorted(occ.items(), key=lambda x:x[1], reverse=True)[:k]
+       
+        return [n[0] for n in top_items]
