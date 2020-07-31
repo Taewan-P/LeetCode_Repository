8db15154ab9bf273acf85b9945d368ca93555da6
Taewan Park <swimingtw@gmail.com>
2020-07-31 17:29:50 +0900

    Add combination sum iii solution in Python

---
+++ summer_coding/216_combination_sum_iii.py
@@ -0,0 +1,12 @@
+from itertools import combinations
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        result = []
+        for tup in combinations([_ for _ in range(1,10)], k):
+            t = sorted(list(tup))
+            if sum(tup) == n:
+                if len(set(tup)) == len(tup):
+                    if not t in result:
+                        result.append(t)
+                    
+        return result
