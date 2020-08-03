863bf84afe3e2b4f2a0c24ed23a30cf0a37ef43f
Taewan Park <swimingtw@gmail.com>
2020-08-03 21:27:25 +0900

    Add shuffle string solution in Python

---
+++ summer_coding/1528_shuffle_string.py
@@ -0,0 +1,7 @@
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str:
+        result = [None] * len(s)
+        for i, j in enumerate(indices):
+            result[j] = s[i]
+            
+        return "".join(result)
